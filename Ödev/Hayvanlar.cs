0a676aaead8c248118bc5d32501291b0bc85ac48
furkansayiner <furkansayner9@gmail.com>
2023-08-28 22:15:06 +0300

    Proje dosyası ekle.

---
+++ Ödev/Hayvanlar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Ödev;
+
+abstract class Hayvanlar
+{
+    public void Ses()
+    {
+        Console.WriteLine("Sesler:");
+    }
+    public abstract void Sescikar();
+    
+}
+class bird : Hayvanlar
+{
+    public override void Sescikar()
+    {
+        Console.WriteLine("Kuşlar Cikcikler");
+    }
+}
+class dog : Hayvanlar
+{
+public override void Sescikar()
+    {
+        Console.WriteLine("Köpekler Havlar");
+    }
+}
+
+class cat : Hayvanlar
+{
+    public override void Sescikar()
+    {
+        Console.WriteLine("Kediler Miyavlar");
+    }
+
+}
+
---
+++ Ödev/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ödev;
+
+//Hayvanlar hayvanlar = new Hayvanlar();
+//hayvanlar.Sescikar();
+//Console.WriteLine("Hayvan sesleri");
+
+bird kus = new bird();
+kus.Sescikar();
+Console.WriteLine("");
+
+dog kopek = new dog();
+kopek.Sescikar();
+Console.WriteLine("");
+
+cat kedi = new cat();
+kedi.Sescikar();
+Console.WriteLine();
